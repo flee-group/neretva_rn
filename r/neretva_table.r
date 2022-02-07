199afe13ceb384bd0395b94b1e85d194d328d795
Matt Talluto <matthew.talluto@uibk.ac.at>
2022-02-07 16:11:45 +0100

    forgotten source file for v0.1.0

---
+++ r/neretva_table.r
@@ -0,0 +1,40 @@
+library(raster)
+library(watershed)
+library(Matrix)
+
+ca = raster("output/catchment.tif")
+neretva = stack("output/neretva.grd")
+neretva = crop(neretva, ca)
+neretva = addLayer(neretva, ca)
+Tp = pixel_topology(neretva)
+Tp = as(Tp, "dgTMatrix")
+Tp_df = data.frame(us = Tp@i+ 1, ds = Tp@j + 1, length_m = Tp@x)
+
+ner_pts = rasterToPoints(neretva)
+ner_pts = as.data.frame(ner_pts)
+ner_pts$id_row = 1:nrow(ner_pts)
+ner_pts = ner_pts[complete.cases(ner_pts),]
+ner_pts$id = 1:nrow(ner_pts)
+ner_pts = merge(ner_pts, Tp_df, by.x = "id_row", by.y = "us", all.x = TRUE)
+
+### find outlet, define length from midpoint of pixel out
+i = which(is.na(ner_pts$ds))
+# distance across a pixel in a cardinal direction
+straight = min(ner_pts$length_m, na.rm=TRUE)
+# distance across a pixel diagonally
+diagon = max(ner_pts$length_m, na.rm=TRUE)
+
+out_direction = ner_pts[i,"drainage"]
+# even number drainages are straight, odd diagonal
+# divide by two because only half a pixel for the outlet
+ner_pts$length_m[i] = ifelse(out_direction %% 2, diagon, straight)/2
+
+# redefine the downstream pixel ids in terms of the 'new' row numbers, not incluing non-wet pixels
+ner_pts$downstream_id = with(ner_pts, id[match(ds, id_row)])
+
+# drop and rename columns, then save
+cols = c("id", 'stream', 'x', 'y', 'accum', 'drainage', 'catchment.2', 'length_m', 'downstream_id')
+ner_pts = ner_pts[, cols]
+colnames(ner_pts)[c(2, 7)] = c("reach_id", "catch_area_km2")
+ner_pts$catch_area_km2 = ner_pts$catch_area_km2 / (1000^2)
+saveRDS(ner_pts, "output/neretva_pixels.rds")
